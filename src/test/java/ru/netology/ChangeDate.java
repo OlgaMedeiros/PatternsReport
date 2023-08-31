c7986c059683ba02fa7c4998cf0e19bb340961d7
OlgaMed <mama2482@mail.ru>
2023-08-31 12:43:12 +0400

    ÐÐÐnewproject

--- src/test/java/ru/netology/ChangeDate.java
+++ src/test/java/ru/netology/ChangeDate.java
@@ -1,2 +1,61 @@
-package ru.netology;public class ChangeDate {
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+import lombok.val;
+
+
+import java.nio.file.attribute.UserPrincipal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+public class ChangeDate {
+    private ChangeDate() {
+    }
+
+    public static String generateDate(int days) {
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String generateCity(String locale) {
+        Faker faker = new Faker();
+        String city = faker.address().city();
+        return city;
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker();
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            Locale localeRu = new Locale("ru");
+            Faker faker = new Faker(localeRu);
+            String city = faker.address().city();
+            String name = faker.name().fullName();
+            String phone = faker.phoneNumber().phoneNumber();
+            UserInfo user = new UserInfo(city, name, phone);
+            return user;
+        }
+    }
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
 }
+
--- src/test/java/ru/netology/PatternsTest.java
+++ src/test/java/ru/netology/PatternsTest.java
@@ -1,2 +1,87 @@
-package ru.netology;public class PatternsTest {
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.conditions.ExactText;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+import jdk.jfr.Configuration;
+//import org.jsoup.select.Evaluator;
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.concurrent.Callable;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PatternsTest {
+    private static Faker faker;
+    // int days = 4;
+    // ChangeDate changeDate = new ChangeDate();
+
+    @BeforeAll
+    static void setFaker() {
+        faker = new Faker(new Locale("ru"));
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+
+
+        var validUser = ChangeDate.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = ChangeDate.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = ChangeDate.generateDate(daysToAddForSecondMeeting);
+
+
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(Condition.visible, Duration.ofSeconds(20));
+// Перепланирование встречи
+
+        $(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=replan-notification] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $$("button").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldBe(Condition.visible, Duration.ofSeconds(20));
+    }
 }
+
+
